24345cdef31a0da817d0af9e095eda8f65ac2cfb
MrBStones <bjornmogelhoj@gmail.com>
2024-12-02 12:48:49 +0100

    max games

--- homework/trialexam.sql
+++ homework/trialexam.sql
@@ -1,45 +1,7 @@
-select count(*) as a
-from (
-    select p.pid
-    from person p
-    where p.birthyear < 1980
-    UNION
-    SELECT pl.pid
-    from plays pl
-    join game g on pl.gid = g.gid
-    where g.did = 5
-) X;
-
-select count(*) as b
-from (
-    select p.pid
-    from person p
-    where p.birthyear < 1980
-    EXCEPT
-    SELECT pl.pid
-    from plays pl
-    join game g on pl.gid = g.gid
-    where g.did = 5
-) X;
-
-select count(*) as c
-from person p
-where p.birthyear < 1980
-or p.pid in (
-    SELECT pl.pid
-    from plays pl
-    join game g on pl.gid = g.gid
-    where g.did = 5
+select MAX(a) FROM (
+    SELECT count(b.pid) as a
+    from buys b
+    join person p on b.pid = p.pid
+    where p.birthyear < b.year
+    GROUP by b.pid
 );
-
-select count(*) as d
-from (
-    select p.pid
-    from person p
-    where p.birthyear < 1980
-    INTERSECT
-    SELECT pl.pid
-    from plays pl
-    join game g on pl.gid = g.gid
-    where g.did = 5
-) X;
