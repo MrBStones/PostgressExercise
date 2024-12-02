5d5a6faf4ac956490134d0f9e857a1fbdc4109ff
MrBStones <bjornmogelhoj@gmail.com>
2024-12-02 13:15:49 +0100

    nineteen ninety

--- homework/trialexam.sql
+++ homework/trialexam.sql
@@ -1,6 +1,6 @@
-select count(*) from (
-    select g.did
-    from game g
-    group by g.did
-    HAVING count(DISTINCT g.genre) = (SELECT DISTINCT count(genre) from game)
-);
+select count(DISTINCT g.did)
+from game g
+join buys b on g.gid = b.gid
+join person p  on b.pid = p.pid
+where p.birthyear = 1990
+;
